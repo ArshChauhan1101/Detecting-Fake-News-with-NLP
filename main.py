610fce4ce080e5e9faea089c8f371dd6d362cabf
ArshChauhan1101 <achauhan4830@conestogac.on.ca>
2024-08-21 23:54:51 -0400

    Trying to do the code in Jupyter Notebook

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import numpy as np
 import pandas as pd
+
+
+fake_news = pd.read_csv('C:\\Users\\ac110\\OneDrive\\Desktop\\Projects\\Detecting-Fake-News-with-NLP\\Data\\Fake.csv')
+true_news = pd.read_csv('C:\\Users\\ac110\\OneDrive\\Desktop\\Projects\\Detecting-Fake-News-with-NLP\\Data\\True.csv')
+fake_news.head()
